c8125f3c5f32a35d004dddd1e230414c66abdfb3
pierocor <piero.coronica@gmail.com>
2018-01-06 18:41:14 +0100

    working BT: insert, naive_print

--- c++/main.cc
+++ c++/main.cc
@@ -3,7 +3,12 @@
 
 int main(){
   Tree<int, int> BT{};
-  BT.insert(1,2);
+  BT.insert(4,7);
+  BT.insert(8,-2);
+  BT.insert(5,1);
+  BT.insert(3,4);
+  BT.insert(6,1);
+  BT.insert(7,4);
   BT.naive_print();
   return 0;
 }
