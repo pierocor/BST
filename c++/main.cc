9a0f7874df4866d4749567799e6a11c3d7d897b3
pierocor <piero.coronica@gmail.com>
2018-02-19 17:59:25 +0100

    find 2 is way better

--- c++/main.cc
+++ c++/main.cc
@@ -6,9 +6,9 @@
 #endif
 
 #include <vector>
-#define MAX_SIZE 100000
+#define MAX_SIZE 50000000
 #define STEP 5000
-#define NLOOK 10000000
+#define NLOOK 100000
 
 #include <time.h>
 #include <ctype.h>
@@ -35,16 +35,17 @@ struct comp{
 };
 
 int main(){
-  double t_start, t_stop;
+  double t_start, t_stop, t_1, t_2;
   std::vector<int> keys;
   size_t size, i, j;
   Tree<int, int, comp<int> > BT{}, T;
   int tmp;
   srand (SEED);
+  std::cout << "#NLOOK: " << NLOOK << "\n#size:\tfind:\tfind2:\t(sec)\n";
   for ( i = 0; i < MAX_SIZE; ++i ){ // ordered vector
     keys.push_back(i);
   }
-  for (size = STEP; size < MAX_SIZE; size += STEP ){
+  for (size = 1024; size < MAX_SIZE; size *= 2 ){
     for ( i = 0; i < size; ++i ){ // shuffle!
       j = rand()%(size - i) + i;
       tmp = keys[i];
@@ -56,12 +57,26 @@ int main(){
       BT.insert( keys[i], rand()%100 );
     }
 
+    for ( i = 0; i < size; ++i ){ // shuffle!
+      j = rand()%(size - i) + i;
+      tmp = keys[i];
+      keys[i] = keys[j];
+      keys[j] = tmp;
+    }
     t_start=cclock();
     for ( i = 0; i < NLOOK; ++i ){
-      BT.find2( keys[ rand()%size ] );
+      BT.find( keys[ i%size ] );
     }
     t_stop=cclock();
-    std::cout << "size: " << size << "\tNLOOK: " << NLOOK << " in " << t_stop - t_start << " sec.\n";
+
+    t_1 = t_stop - t_start;
+    t_start=cclock();
+    for ( i = 0; i < NLOOK; ++i ){
+      BT.find2( keys[ i%size ] );
+    }
+    t_stop=cclock();
+    t_2 = t_stop - t_start;
+    std::cout << size << "\t" << t_1 << "\t" << t_2 << "\n";
     BT.clean();
   }
   return 0;
