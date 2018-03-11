543ac0ac3ae63c29b8f285d7a3f88d20620bc470
pierocor <piero.coronica@gmail.com>
2018-03-11 16:50:15 +0100

    __init__

--- python/exam_requests.py
+++ python/exam_requests.py
@@ -1,11 +1,11 @@
 '''
-Exam requests: 
+Exam requests:
 
-1. Write a class PostcardList that satisfies the tests (defined using unittest). 
+1. Write a class PostcardList that satisfies the tests (defined using unittest).
 2. OPTIONAL) Populate and save one (or more) file(s) to test other groups implementations using unittest.
 
-SPECIFICATIONS: 
-Class PostcardList reads/writes Postcard messages from/to a properly formatted file: each record (i.e., file's line) is a Postcard. 
+SPECIFICATIONS:
+Class PostcardList reads/writes Postcard messages from/to a properly formatted file: each record (i.e., file's line) is a Postcard.
 The Postcard format is "date:$(DATE); from:$(SENDER); to:$(RECEIVER);" (e.g., "date:2009-12-24; from:Daisy; to:Donald;"). For the sake of simplicity, the message is not considered.
 
 Class PostcardList must have at least these attributes:
@@ -15,15 +15,15 @@ Class PostcardList must have at least these attributes:
  - _from: is a dict where the key is the string sender, and the value is a list of indices. Each index refers to the corresponding record.
  - _to:   is a dict where the key is the string receiver, and the value is a list of indices. Each index refers to the corresponding record.
 
-Class PostcardList must manage the I/O file through the following member functions. Note that you are free to choose the appropriate arguments, but the function names must be as follows:  
- - writeFile(self,...): write self.{_date,_from,_to} to self._file  
- - readFile(self,...): from self._file read self.{_date,_from,_to}    
- - parsePostcards(self,...): parse self._postcards, set self.{_date,_from,_to} 
- - updateFile(self,...): as write but appending to self._file 
- - updateLists(self,...): as read but appending to self._postcards 
+Class PostcardList must manage the I/O file through the following member functions. Note that you are free to choose the appropriate arguments, but the function names must be as follows:
+ - writeFile(self,...): write self.{_date,_from,_to} to self._file
+ - readFile(self,...): from self._file read self.{_date,_from,_to}
+ - parsePostcards(self,...): parse self._postcards, set self.{_date,_from,_to}
+ - updateFile(self,...): as write but appending to self._file
+ - updateLists(self,...): as read but appending to self._postcards
  - getNumberOfPostcards(self): returns length of self._postcards
 
-Class PostcardList must manage the sorting of dates/senders/receivers. Note that the names and arguments of the functions are fixed as follows: 
+Class PostcardList must manage the sorting of dates/senders/receivers. Note that the names and arguments of the functions are fixed as follows:
  - getPostcardsByDateRange(self,date_range): returns the postcards within a date_range
  - getPostcardsBySender(self, sender): returns the postcards from a sender
  - getPostcardsByReceiver(self, receiver): returns the postcards to a receiver
@@ -32,16 +32,32 @@ Class PostcardList must manage the sorting of dates/senders/receivers. Note that
 import unittest
 import datetime  # use this module to deal with dates:  https://docs.python.org/3/library/datetime.html
 
-class PostcardList(object): 
+class PostcardList(object):
+    """
+    Class PostcardList reads/writes Postcard messages from/to a properly formatted file: each record (i.e., file's line) is a Postcard.
+    The Postcard format is "date:$(DATE); from:$(SENDER); to:$(RECEIVER);" (e.g., "date:2009-12-24; from:Daisy; to:Donald;"). For the sake of simplicity, the message is not considered.
+    """
+    def __init__(self):
+        self._file = []
+        self._postcards = []
+        self._date = []
+        self._from = []
+        self._to = []
+        print("\nPostcard list created\n")
+    def __del__(self):
+        print("\ndelete PostcardList\n")
+    def __str__(self): # automatically called by print
+        print(self._file)
+
     ########################
     # define attributes here
-    pass
+    # pass
     ########################
 
 
 ########################
-# TO COMMENT 
-from exam_solution import PostcardList # overrides the class define above
+# TO COMMENT
+# from exam_solution import PostcardList # overrides the class define above
 ########################
 
 
@@ -54,7 +70,7 @@ class Test(unittest.TestCase):
         self._PstLst = [PostcardList() for f in self._files]
         [p.readFile(f) for f,p in zip(self._files,self._PstLst)]
 
-    def test_missing_attributes(self): 
+    def test_missing_attributes(self):
         attributes_to_define = ['populatePostcards',\
                                 '_file','_postcards','_from','_to',\
                                 'writeFile','readFile','parsePostcards',\
@@ -64,22 +80,22 @@ class Test(unittest.TestCase):
                                 'getPostcardsBySender',\
                                 'getPostcardsByReceiver']
         for attr in attributes_to_define:
-            if attr not in dir(self._PstLst[0]): 
+            if attr not in dir(self._PstLst[0]):
                 raise Exception(attr+' is missing')
 
-    def test_check_getPostcardByDateRange(self): 
+    def test_check_getPostcardByDateRange(self):
         dr_test = []
-        for i in [0,1,4,6]: 
+        for i in [0,1,4,6]:
             dr_test.append(\
                 self._PstLst[i].getPostcardsByDateRange(date_range=\
                    (datetime.datetime.strptime('2008-1-1', "%Y-%m-%d"),\
                     datetime.datetime.strptime('2010-12-31', "%Y-%m-%d"))))
-        self.assertListEqual(sorted(dr_test[0]),sorted(['date:2010-06-23; from:Sneezy; to:Alice;\n', 'date:2009-12-12; from:Dopey; to:Peter;\n', 'date:2008-03-23; from:Sneezy; to:Pluto;\n', 'date:2008-06-03; from:Goofy; to:Pluto;\n'])) 
+        self.assertListEqual(sorted(dr_test[0]),sorted(['date:2010-06-23; from:Sneezy; to:Alice;\n', 'date:2009-12-12; from:Dopey; to:Peter;\n', 'date:2008-03-23; from:Sneezy; to:Pluto;\n', 'date:2008-06-03; from:Goofy; to:Pluto;\n']))
         self.assertEqual(len(dr_test[1]),1)
         self.assertListEqual(sorted(dr_test[2]),sorted(['date:2010-03-30; from:Louie; to:Sneezy;\n', 'date:2010-03-05; from:Goofy; to:Dopey;\n', 'date:2009-11-08; from:Daisy; to:Minnie;\n', 'date:2010-07-13; from:Bashful; to:Louie;\n', 'date:2008-06-29; from:Huey; to:Dopey;\n', 'date:2009-01-04; from:Alice; to:Hook;\n', 'date:2009-04-28; from:Bashful; to:Mickey;\n']))
         self.assertEqual(len(dr_test[3]),7)
 
-    def test_check_getPostcardBySender(self): 
+    def test_check_getPostcardBySender(self):
         s_test = []
         for i in [2,3,5,7]:
             for sender in ['Daisy','Bashful','Peter']:
@@ -91,7 +107,7 @@ class Test(unittest.TestCase):
         self.assertEqual(len(s_test[5]),0)
         self.assertGreater(len(s_test[8]),len(s_test[9]))
 
-    def test_check_getPostcardByReceiver(self): 
+    def test_check_getPostcardByReceiver(self):
         r_test = []
         for i in [0,3,8,9]:
             for receiver in ['Peter','Hook','Alice','SnowWhite']:
@@ -102,7 +118,7 @@ class Test(unittest.TestCase):
         self.assertEqual(len(r_test[2]),2)
         self.assertEqual(len(r_test[6]),3)
 
-    def test_check_sent_received_when(self): 
+    def test_check_sent_received_when(self):
         srw_test = []
         for ii in range(10):
             for jj in ["Mickey","Minnie","Goofy","Pluto","Donald","Daisy","$crooge","Huey","Dewey","Louie","Peter","Hook","Alice","SnowWhite","Doc","Sleepy","Sneezy","Bashful","Dopey","Happy","Grumpy"]:
@@ -121,4 +137,3 @@ class Test(unittest.TestCase):
 if __name__ == '__main__':
 
     unittest.main()
-
